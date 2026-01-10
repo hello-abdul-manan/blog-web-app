801a5c26ae96800c0d395db14fd621d9eda0fb67
hello-abdul-manan <helloabdulmanan@gmail.com>
2026-01-10 23:38:02 +0500

    Create models for category and post

--- blog_app/blog/admin.py
+++ blog_app/blog/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from .models import Category, Post
 
 # Register your models here.
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('title', 'author', 'category', 'created_at')
+    list_filter = ('category', 'created_at')
+    search_fields = ('title', 'content')
+
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('name', 'slug')
+    prepopulated_fields = {'slug': ('name',)}
--- blog_app/blog/models.py
+++ blog_app/blog/models.py
@@ -1,3 +1,22 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+    slug = models.SlugField(unique=True)
+
+    def __str__(self):
+        return self.name
+    
+class Post(models.Model):
+    title = models.CharField(max_length=100)
+    content = models.TextField()
+    category = models.ForeignKey(Category, on_delete=models.SET_NULL, null=True, blank=True)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.title
+    
