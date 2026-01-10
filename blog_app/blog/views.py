efe1b59df90a61064bcadd46d0267ba9ee8245f1
hello-abdul-manan <helloabdulmanan@gmail.com>
2026-01-11 00:46:41 +0500

    Create a view for post list

--- blog_app/blog/views.py
+++ blog_app/blog/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from .models import Post
 
 # Create your views here.
+def post_list(request):
+    posts = Post.objects.all().order_by('-created_at')
+    return render(request, 'blog/blog_list.html', {'posts': posts})
