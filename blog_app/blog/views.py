76a8d10699545a03cb6cbc84226c7f7fafd194c3
hello-abdul-manan <helloabdulmanan@gmail.com>
2026-01-11 16:24:07 +0500

    Add a view to create posts

--- blog_app/blog/views.py
+++ blog_app/blog/views.py
@@ -1,7 +1,20 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Post
+from .forms import PostForm
 
 # Create your views here.
 def post_list(request):
     posts = Post.objects.all().order_by('-created_at')
     return render(request, 'blog/blog_list.html', {'posts': posts})
+
+def create_post(request):
+    if request.method == 'POST':
+        form = PostForm(request.POST)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.author = request.user
+            post.save()
+            return redirect('post_list')
+    else:
+        form = PostForm()
+    return render(request, 'blog/create_post.html', {'form': form})
