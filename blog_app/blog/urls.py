b8b18398f7ca71c2c6375b5a048a2d992dfba5e9
hello-abdul-manan <helloabdulmanan@gmail.com>
2026-01-11 16:26:09 +0500

    Add url for creating posts

--- blog_app/blog/urls.py
+++ blog_app/blog/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import post_list
+from .views import post_list, create_post
 
 urlpatterns = [
-    path('post_list', post_list, name='post_list')
+    path('post_list/', post_list, name='post_list'),
+    path('create/', create_post, name='create_post')
 ]
