bb213ab77501e7b80b2b1d1ea13b9bbc44dfa421
hello-abdul-manan <helloabdulmanan@gmail.com>
2026-01-13 00:03:01 +0500

    Add urls for authentication

--- blog_app/blog/urls.py
+++ blog_app/blog/urls.py
@@ -1,10 +1,13 @@
 from django.urls import path
-from .views import home, post_list, create_post, update_post, delete_post
+from .views import home, post_list, create_post, update_post, delete_post, signup_view, login_view, logout_view
 
 urlpatterns = [
     path('', home, name='home'),
     path('post_list/', post_list, name='post_list'),
     path('create/', create_post, name='create_post'),
     path('post/<int:post_id>/edit/', update_post, name='update_post'),
-    path('post/<int:post_id>/delete/', delete_post, name='delete_post')
+    path('post/<int:post_id>/delete/', delete_post, name='delete_post'),
+    path('signup/', signup_view, name='signup'),
+    path('login/', login_view, name='login'),
+    path('logout/', logout_view, name='logout'),
 ]
