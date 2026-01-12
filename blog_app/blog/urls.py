fa3e75d93679542a7f362adf98c30931f350ac67
hello-abdul-manan <helloabdulmanan@gmail.com>
2026-01-12 19:46:57 +0500

    Add url to delete posts

--- blog_app/blog/urls.py
+++ blog_app/blog/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import home, post_list, create_post, update_post
+from .views import home, post_list, create_post, update_post, delete_post
 
 urlpatterns = [
     path('', home, name='home'),
     path('post_list/', post_list, name='post_list'),
     path('create/', create_post, name='create_post'),
-    path('post/<int:post_id>/edit/', update_post, name='update_post')
+    path('post/<int:post_id>/edit/', update_post, name='update_post'),
+    path('post/<int:post_id>/delete/', delete_post, name='delete_post')
 ]
