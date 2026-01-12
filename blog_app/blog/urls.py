6fcc83aa439a6c8efa8abd31e098eae3ab13e3c9
hello-abdul-manan <helloabdulmanan@gmail.com>
2026-01-12 19:13:08 +0500

    Add urls for home page and updating posts

--- blog_app/blog/urls.py
+++ blog_app/blog/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
-from .views import post_list, create_post
+from .views import home, post_list, create_post, update_post
 
 urlpatterns = [
+    path('', home, name='home'),
     path('post_list/', post_list, name='post_list'),
-    path('create/', create_post, name='create_post')
+    path('create/', create_post, name='create_post'),
+    path('post/<int:post_id>/edit/', update_post, name='update_post')
 ]
