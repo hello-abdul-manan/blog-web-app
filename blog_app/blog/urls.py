b169d10dd40f7f9a10172790ddde6fdeeb0ad28f
hello-abdul-manan <helloabdulmanan@gmail.com>
2026-01-13 15:07:38 +0500

    Add url for post detail page

--- blog_app/blog/urls.py
+++ blog_app/blog/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
-from .views import home, post_list, create_post, update_post, delete_post, signup_view, login_view, logout_view
+from .views import home, post_list, create_post, update_post, delete_post, signup_view, login_view, logout_view, post_detail
 
 urlpatterns = [
     path('', home, name='home'),
     path('post_list/', post_list, name='post_list'),
     path('create/', create_post, name='create_post'),
+    path('post/<int:post_id>/', post_detail, name='post_detail'),
     path('post/<int:post_id>/edit/', update_post, name='update_post'),
     path('post/<int:post_id>/delete/', delete_post, name='delete_post'),
     path('signup/', signup_view, name='signup'),
